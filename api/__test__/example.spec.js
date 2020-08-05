5e99ffda1d5b2cf6dd016e7ec38f7d44d06cfca6
Ekaterina <nataveres@gmail.com>
2020-08-05 20:31:53 +0200

    Final test commit

--- api/__test__/example.spec.js
+++ api/__test__/example.spec.js
@@ -2,15 +2,21 @@ const app = require("../server"); // Link to server file
 const supertest = require("supertest");
 const request = supertest(app);
 const assert = require("assert");
+const User = require('../models/User');
 
-// Jest cheat sheet: https://devhints.io/jest
+// Jest cheat sheet: https://devhints.io/jestnpmn
 
 describe("TEST /api/home", function () {
   it("gets the home endpoint", async (done) => {
     //const response = await request.get("/api/home");
-    supertest(app).get("/api/home").expect(200);
+    supertest(app).get("/api/home").expect(200).expect('Content-Type', /json/);
     //expect(response.status).toBe(200);
     //expect(response.text).toBeDefined();
+    done();
+  });
+
+  it("gets the content length", async (done) => {
+    supertest(app).get("/api/home").expect('Content-Length', '8');
     done();
   });
 });
@@ -22,11 +28,9 @@ describe("TEST /api/contacts", function () {
     done();
   });
 
-  it("responds with json", async function (done) {
-    supertest(app)
-      .get("/api/contacts")
-      //.expect("Content-Type", /text/)
-      .expect(200, done);
+  it("responds with json", async (done) => {
+    supertest(app).get("/api/contacts").expect('Content-Type', /json/);
+    done();
   });
 
   it("post a contact test", function (done) {
@@ -34,22 +38,51 @@ describe("TEST /api/contacts", function () {
       .post("/api/contacts/")
       .send({
         Name: "John",
-        Email: "john@eample.com",
+        Email: "john@example.com",
         Subject: "Question",
-        Message: "Do you have child glasses?",
+        Message: "Do you have child glasses?"
       })
-      .expect("Content-Type", "text/html; charset=utf-8")
-      .expect(200)
-      .end(function (err, res) {
-        if (err) return done(err);
-        done();
-      });
+      .expect("Content-Type", /text/)
+      .expect(200);
+    done();
   });
 });
 
 describe("TEST /api/products", function () {
-  it("gets the contacts endpoint", async (done) => {
+  it("gets the products endpoint", async (done) => {
     supertest(app).get("/api/products").expect(200);
+    done();
+  });
+
+  it("responds with json", async (done) => {
+    supertest(app).get("/api/products").expect('Content-Type', /json/);
+    done();
+  });
+})
+
+describe("TEST /api/services", function () {
+  it("gets the services endpoint", async (done) => {
+    supertest(app).get("/api/services").expect(200);
+    done();
+  });
+
+  it("responds with json", async (done) => {
+    supertest(app).get("/api/services").expect('Content-Type', /json/);
+    done();
+  });
+})
+
+describe("TEST /api/login", function () {
+  it("doesn't get the login endpoint", async (done) => {
+    supertest(app).get("/api/login").expect(404);
+    done();
+  });
+
+  it("does get login endpoint", async (done) => {
+    supertest(app).get("/api/login").send({
+      email: 'rashaali84@gmail.com',
+      pass: 123
+    }).expect(200);
     done();
   });
 })
